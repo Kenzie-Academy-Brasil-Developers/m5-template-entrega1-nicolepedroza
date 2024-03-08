a8746179d63a457dc7f3b8127848887610afa5fa
nicolepedroza <nicolemoniquepedroza@hotmail.com>
2024-03-08 19:11:20 -0300

    feat: deploy

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import "dotenv/config"
 import "express-async-errors"
+import cors from "cors"
 import express, { json } from "express";
 import helmet from "helmet";
 import { taskRouter } from "./routes/task.routes";
@@ -9,13 +10,15 @@ import { userRouter } from "./routes/user.routes";
 
 export const app = express();
 
+app.use(cors());
+
 app.use(helmet());
 
 app.use(json());
 
-app.use("/tasks", taskRouter)
+app.use("/tasks", taskRouter);
 
-app.use("/categories", categoryRouter)
+app.use("/categories", categoryRouter);
 
-app.use("/users", userRouter)
-app.use(handleErrors)
+app.use("/users", userRouter);
+app.use(handleErrors);
